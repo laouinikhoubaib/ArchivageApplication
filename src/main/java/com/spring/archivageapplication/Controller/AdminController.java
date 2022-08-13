b924d111ba7d59fa09ab7adcfd50ed0dad4984a2
laouinikhoubaib <73175459+laouinikhoubaib@users.noreply.github.com>
2022-08-13 01:37:29 +0100

    maj authorities

--- src/main/java/com/spring/archivageapplication/Controller/AdminController.java
+++ src/main/java/com/spring/archivageapplication/Controller/AdminController.java
@@ -2,20 +2,27 @@ package com.spring.archivageapplication.Controller;
 
 
 
+import com.spring.archivageapplication.Models.Complaint;
 import com.spring.archivageapplication.Models.User;
 import com.spring.archivageapplication.Repository.userRepository;
+import com.spring.archivageapplication.Service.Complaint.ComplaintService;
 import com.spring.archivageapplication.Service.User.userService;
-//import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import javax.mail.MessagingException;
+import javax.transaction.Transactional;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
 @CrossOrigin(origins = "http://localhost:4200")
+@PreAuthorize("hasRole('SuperAdmin') or hasRole('Admin')")
+
 public class AdminController {
 
     userService us;
@@ -27,6 +34,8 @@ public class AdminController {
 
         this.us = us;
     }
+    @Autowired
+    ComplaintService ComplaintService;
 
     @PostMapping("/add")
     public ResponseEntity<User> create(@RequestBody  User user) {
@@ -97,6 +106,24 @@ public class AdminController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    @GetMapping("/retrieveAllComplaints")
+    public List<Complaint> retrieveAllComplaints() {
 
+        return ComplaintService.retrieveAllComplaints();
+    }
+
+
+    @PutMapping("/updateUntreatedComplaint/{Compalaint-id}")
+    @Transactional
+    public Complaint updateComplaint2(@PathVariable(value ="Compalaint-id")Integer id) throws MessagingException {
+        return ComplaintService.updateComplaint2(id);
+    }
+
+    @PostMapping("/affectatComplaintToUser/{Complaint_id}/{id}")
+    @ResponseBody
+    public void affectatComplaintToUser(@PathVariable("Complaint_id") int Complaint_id,@PathVariable("id")int id) throws IOException {
+
+        ComplaintService.affectatComplaintToUser(Complaint_id, id);
+    }
 
 }
